c340b08bf8ec2ca89aa88d75fefff052de098582
Matthew Burket <m@tthewburket.com>
2018-10-21 14:47:25 -0500

    Formatting for audio.h

--- audio.h
+++ audio.h
@@ -8,8 +8,8 @@
 
 class Audio {
 public:
-     static void generate_tone(double freq, std::vector<double> &vector, long length);
-     static void generate_afsk(std::vector<double> &vector, std::vector<bool> &message);
-     static void generate_dual_tone(double freq1, double freq2, std::vector<double> &vector, long length);
+    static void generate_tone(double freq, std::vector<double> &vector, long length);
+    static void generate_afsk(std::vector<double> &vector, std::vector<bool> &message);
+    static void generate_dual_tone(double freq1, double freq2, std::vector<double> &vector, long length);
 };
 #endif //SAMEENCODER_AUDIO_H
