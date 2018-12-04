98ef1cfcf4f2d05614e1c2caa611ae5c912fbd20
Matthew Burket <m@tthewburket.com>
2018-12-04 17:15:38 -0600

    More refactoring

---
+++ UI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mburket on 12/4/18.
+//
+
+#include "UI.h"
---
+++ UI.h
@@ -0,0 +1,16 @@
+//
+// Created by mburket on 12/4/18.
+//
+
+#ifndef SAMEENCODER_UI_H
+#define SAMEENCODER_UI_H
+
+
+
+class UI {
+
+};
+
+
+
+#endif //SAMEENCODER_UI_H
